0ac5e9e9aa2ef046f55d55351a2b74afe38e340a
geniuscc78 <geniuscc7@163.com>
2017-10-31 16:14:17 +0800

    end of chapter11

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,10 +10,10 @@ class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(' \n')
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        time.sleep(0.5)
+        time.sleep(3)
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
@@ -23,9 +23,9 @@ class ItemValidationTest(FunctionalTest):
 
         self.check_for_row_in_list_table('1: Buy milk')
 
-        self.get_item_input_box().send_keys('\n')
+        self.get_item_input_box().send_keys(' \n')
         self.get_item_input_box().send_keys(Keys.ENTER)
-        time.sleep(0.5)
+        time.sleep(3)
 
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
